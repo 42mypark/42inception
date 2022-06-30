7acb525fd2e38d43ce82f023de32909f4a93e12d
Myeonghyeon Park <mypark@c1r1s5.42seoul.kr>
2022-06-30 14:49:10 +0900

    substract mask

--- set_server_ip.sh
+++ set_server_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-IP=$(ip address | grep -e ".*\.*\..*\..*/* enp0s3" | awk '{print $2;}');
+IP=$(ip address | grep -e ".*\.*\..*\..*/* enp0s3" | awk '{print $2;}' | sed 's/\/.*//');
 sed -i "s#localhost#${IP}#g" srcs/php-fpm/.php-fpm.env;
