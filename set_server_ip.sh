498e872e5c58f58cf46c5d55aa5441b30adcb0ae
Myeonghyeon Park <mypark@c1r1s5.42seoul.kr>
2022-06-30 14:57:56 +0900

    nginx run.sh localhost -> ip

--- set_server_ip.sh
+++ set_server_ip.sh
@@ -2,3 +2,4 @@
 IP=$(ip address | grep -e ".*\.*\..*\..*/* enp0s3" | awk '{print $2;}' | sed 's/\/.*//');
 sed -i "s#localhost#${IP}#g" srcs/php-fpm/.php-fpm.env;
 sed -i "s#localhost#${IP}#g" srcs/nginx/default.conf;
+sed -i "s#localhost#${IP}#g" srcs/nginx/script/run.sh;
