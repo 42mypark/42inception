1cf4322debdd77a977df1bbba2d5bd63392ef260
mypark <skyttl34@gmail.com>
2022-07-05 16:32:13 +0900

    edit init_vm.sh

--- init_vm.sh
+++ init_vm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt update && apt install -y openssh-server;
 apt install -y curl vim docker.io docker-compose make;
-echo "127.0.0.1 mypark.42cadet.kr" >> /etc/hosts;
+CHECHK_HOSTS=$(grep 42cadet.fr /etc/hosts);
+if [ $? -eq 1 ]; then
+	echo "127.0.0.1 mypark.42cadet.fr" >> /etc/hosts;
+fi;
