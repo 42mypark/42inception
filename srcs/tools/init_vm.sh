00ad832f48633ab68c8ca8b446d3e515bde609db
mypark <skyttl34@gmail.com>
2022-07-10 09:11:24 +0900

    도메인 고침

--- srcs/tools/init_vm.sh
+++ srcs/tools/init_vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update && apt install -y openssh-server;
 apt install -y curl vim docker.io docker-compose make;
-CHECHK_HOSTS=$(grep 42cadet.fr /etc/hosts);
+CHECHK_HOSTS=$(grep 42.fr /etc/hosts);
 if [ $? -eq 1 ]; then
-	echo "127.0.0.1 mypark.42cadet.fr" >> /etc/hosts;
+	echo "127.0.0.1 mypark.42.fr" >> /etc/hosts;
 fi;
