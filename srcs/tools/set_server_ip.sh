fcb6ccc7b59a4670df402bed19395124f052cad8
plan5886 <skyttl34@gmail.com>
2022-07-10 15:39:01 +0900

    mkdir materials

--- srcs/tools/set_server_ip.sh
+++ srcs/tools/set_server_ip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 IP=$(ip address | grep -e ".*\.*\..*\..*/* enp*" | awk '{print $2;}' | sed 's/\/.*//');
 sed -i "s#localhost#${IP}#g" srcs/php-fpm/.php-fpm.env;
-sed -i "s#localhost#${IP}#g" srcs/nginx/default.conf;
-sed -i "s#localhost#${IP}#g" srcs/nginx/script/run.sh;
+sed -i "s#localhost#${IP}#g" srcs/nginx/materials/conf/default.conf;
+sed -i "s#localhost#${IP}#g" srcs/nginx/materials/script/run.sh;
