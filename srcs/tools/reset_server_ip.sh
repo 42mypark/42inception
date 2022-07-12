9a10a4396b3746b2f4da3207cda59b93d22a3d1f
plan5886 <skyttl34@gmail.com>
2022-07-12 18:48:32 +0900

    delete files

--- srcs/tools/reset_server_ip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-IP=$(ip address | grep -e ".*\.*\..*\..*/* enp*" | awk '{print $2;}' | sed 's/\/.*//');
-sed -i "s#${IP}#localhost#g" srcs/php-fpm/.php-fpm.env;
-sed -i "s#${IP}#localhost#g" srcs/nginx/materials/conf/default.conf;
-sed -i "s#${IP}#localhost#g" srcs/nginx/materials/script/run.sh;
